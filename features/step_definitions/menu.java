25fc30e6e7ee2936640dca6193985e57b670b36e
Miguel <m.calvoma@alumnos.urjc.es>
2016-02-21 12:41:25 +0100

    Mejora de los .java de los test unitarios

---
+++ features/step_definitions/menu.java
@@ -0,0 +1,191 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+
+public class menu {
+
+	@Given("^a <idPlayer>,  <costPlayer> and <money> team$")
+	public void a_idPlayer_costPlayer_and_money_team() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^I press the \"([^\"]*)\" button AND I don't have enough money$")
+	public void i_press_the_button_AND_I_don_t_have_enough_money(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^should return <status> and money should be <remainingMoney>$")
+	public void should_return_status_and_money_should_be_remainingMoney() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^a <idPlayer>, <costPlayer> and <money> team$")
+	public void a_idPlayer_costPlayer_and_money_team() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^I press the \"([^\"]*)\" button AND I have enough money$")
+	public void i_press_the_button_AND_I_have_enough_money(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^the app menu$")
+	public void the_app_menu() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^I press the \"([^\"]*)\" button$")
+	public void i_press_the_button(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can see the \"([^\"]*)\" screen$")
+	public void i_can_see_the_screen(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can see the Fichajes screen$")
+	public void i_can_see_the_Fichajes_screen() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can share my score$")
+	public void i_can_share_my_score() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^a <user>$")
+	public void a_user() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the fiel <password> is empty and the field <user> is not empty$")
+	public void the_fiel_password_is_empty_and_the_field_user_is_not_empty() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^should return <access>$")
+	public void should_return_access() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^a <password>$")
+	public void a_password() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the fiel <password> is not empty and the field <user> is empty$")
+	public void the_fiel_password_is_not_empty_and_the_field_user_is_empty() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^a <user> and <password>$")
+	public void a_user_and_password() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the <password> is valid and the <user> is invalid$")
+	public void the_password_is_valid_and_the_user_is_invalid() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the <password> is valid and the <user> is valid$")
+	public void the_password_is_valid_and_the_user_is_valid() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^the <password> is invalid and the <user> is valid$")
+	public void the_password_is_invalid_and_the_user_is_valid() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^an user team$")
+	public void an_user_team() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I could see my principal team on screen$")
+	public void i_could_see_my_principal_team_on_screen() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^an empty user team$")
+	public void an_empty_user_team() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^I presss the \"([^\"]*)\" button$")
+	public void i_presss_the_button(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I could see nothing$")
+	public void i_could_see_nothing() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I could see my reserve team on screen$")
+	public void i_could_see_my_reserve_team_on_screen() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I could see my score and money$")
+	public void i_could_see_my_score_and_money() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^a <team> and <idPlayer>$")
+	public void a_team_and_idPlayer() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^it will not appear on <mercado>$")
+	public void it_will_not_appear_on_mercado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^it will appear on <mercado>$")
+	public void it_will_appear_on_mercado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^the menu$")
+	public void the_menu() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^I can see the <mercado>$")
+	public void i_can_see_the_mercado() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
